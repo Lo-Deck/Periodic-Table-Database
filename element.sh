c7f4e65d4cc24d93b2e2c8b537225d878a60ccf6
Lo-Deck <122597745+Lo-Deck@users.noreply.github.com>
2025-07-24 09:26:36 +0200

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-echo -e "\nPlease provide an element as an argument."
+# echo -e "\nPlease provide an element as an argument."
 
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [[ -z $1 ]]
 then
-  echo -e "\nPlease provide an element as an argument."
+  echo "Please provide an element as an argument."
 else
 
   ELEMENT_INFO=""
 
   if  [[ "$1" =~ ^[0-9]+$ ]]
   then
-    # NUMBER=$1
     ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, t.type, p.melting_point_celsius, p.boiling_point_celsius FROM elements AS e JOIN properties AS p ON e.atomic_number = p.atomic_number JOIN types AS t ON p.type_id = t.type_id WHERE e.atomic_number = $1;")
   fi
 
   if [[ -z "$ELEMENT_INFO" && "$1" =~ ^[A-Z][a-z]?$ ]]
   then
-    # LETTER=$1
     ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, t.type, p.melting_point_celsius, p.boiling_point_celsius FROM elements AS e JOIN properties AS p ON e.atomic_number = p.atomic_number JOIN types AS t ON p.type_id = t.type_id WHERE e.symbol = '$1';")
   fi
 
+  # echo $ELEMENT_INFO
+
   if [[ -z "$ELEMENT_INFO" ]]
   then
-    # WORD=$1
     ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, t.type, p.melting_point_celsius, p.boiling_point_celsius FROM elements AS e JOIN properties AS p ON e.atomic_number = p.atomic_number JOIN types AS t ON p.type_id = t.type_id WHERE e.name = '$1';")
   fi
 
